1503fad362f07661b8975058e9a806c1e825564f
JhonnyParraB <jhonnyparra01@hotmail.com>
2020-03-09 22:10:39 -0500

    RAS Setup Project 2

---
+++ src/main/java/co/edu/javeriana/ras/RasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.ras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasApplication.class, args);
+	}
+
+}
